d26df545e3e3bcaac6d3e360a0aa4450ebea9f90
Kiran Kumar Roy <bikky22@users.noreply.github.com>
2017-07-11 19:16:14 +0530

    Create root.sh

---
+++ Group/root.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+sudo -i
+cd
+touch envar
+echo "#my custom bash" >> ~/.bashrc
+echo "echo "Welcome from Kiran Kumar Roy"" >> ~/.bashrc
+echo "echo "SUDO: !! : !!  !! : !! :root"" >> ~/.bashrc
+echo "source ~/envar" >> ~/.bashrc
+echo "echo "Loading.................Done"" >> ~/.bashrc
