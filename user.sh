21a8102bec98b65c2b8966d77ce3829790baec9f
Kiran Kumar Roy <bikky22@users.noreply.github.com>
2017-07-18 19:53:10 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -3,22 +3,28 @@ sudo addgroup compute
 #sudo addgroup hpc
 #sudo addgroup bdt
 #sudo addgroup dbt
+echo "Done adding Groups"
+
 #add user
 sudo adduser analytics
 #sudo adduser bdt
 #sudo adduser hpc
 #sudo adduser database
+echo "Done adding Users"
+
 #change group
 sudo usermod -g compute analytics
 #sudo usermod -g bdt bdt
 #sudo usermod -g hpc hpc
 #sudo usermod -g dbt database
+echo "Done Group Change"
+
 #del group
 sudo delgroup analytics
 #sudo delgroup hadoop
 #sudo delgroup spark
 #sudo delgroup database
-#add folder
+echo "Done Group delete"
 
 #
 #compgen -g
