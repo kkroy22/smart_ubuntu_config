db5c8930c3a2527d7353a400653f981237642f3c
Kiran Kumar Roy <bikky22@users.noreply.github.com>
2017-07-18 19:50:39 +0530

    Update sudo.sh

--- Group/sudo.sh
+++ Group/sudo.sh
@@ -1,44 +1,14 @@
 #! /bin/bash
 
-# set time to local time
-timedatectl set-local-rtc 1
-
 # making iniatial user dir
-cd 
-mkdir -p ~/bundle/launcher
+cd ~
+touch envar
+mkdir -p ~/bundle/scripts
 
 # oh my bash!
-cd 
-touch envar
+echo "__________________________________" >> ~/.bashrc
 echo "#my custom bash" >> ~/.bashrc
 echo "echo "Welcome from Kiran Kumar Roy"" >> ~/.bashrc
 echo "echo "SUDO:::::::::::::::::::bikky"" >> ~/.bashrc
 echo "source ~/envar" >> ~/.bashrc
 echo "echo "Loading.................Done"" >> ~/.bashrc
-
-# install required packages
-pkgs=(  
-         'chromium-browser'
-         'git'
-         'tmux'
-         'jupyter-core'
-         'vim'
-         'r-base'
-         'curl'
-         'ssh'
-         )
-
-echo "Installing core packages"
-sudo apt update
-sudo apt upgrade
-for i in "${pkgs[@]}"
-do
-    sudo apt install $i
-done
-
-
-# rust-lang
-#curl https://sh.rustup.rs -sSf | sh
-
-# cran.r-project
-#sudo apt-get install r-base
