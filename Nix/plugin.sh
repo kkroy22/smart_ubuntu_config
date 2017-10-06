82ec147d6300a25f93eaa78426c688a5a9833590
Kiran Kumar Roy <kkroy22@gmail.com>
2017-10-07 03:15:52 +0530

    new

--- Nix/plugin.sh
+++ Nix/plugin.sh
@@ -9,4 +9,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/bundle/
 
 cd ~
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mv .oh-my-zsh $HOME/bundle/My-Sweet-Little-Sys/Nix/dotfile/zsh/plugins/oh-my-zsh
+mv .oh-my-zsh/ $HOME/bundle/My-Sweet-Little-Sys/Nix/dotfile/zsh/plugins/oh-my-zsh
