383e8804d98a17fe6ac88763f7bb54c4312279a9
Kiran Kumar Roy <kkroy22@gmail.com>
2017-10-07 03:53:16 +0530

    h

--- Nix/dotfile/zsh/zshrc_manager.sh
+++ Nix/dotfile/zsh/zshrc_manager.sh
@@ -8,6 +8,5 @@ else
 fi
 
 echo "Updating configuration"
-#(cd $HOME/bundle/My-Sweet-Little-Sys/Nix/dotfile && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
 (cd $HOME/bundle/My-Sweet-Little-Sys/Nix/dotfile && git pull && git submodule update --init --recursive)
 source $HOME/bundle/My-Sweet-Little-Sys/Nix/dotfile/zsh/zshrc.sh
