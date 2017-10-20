8ffce52142162b804434381af908d869bcee0831
Kiran Kumar Roy <kkroy22@gmail.com>
2017-10-20 13:58:34 +0530

    done changes

--- nix/dotfile/zsh/zshrc_manager.sh
+++ nix/dotfile/zsh/zshrc_manager.sh
@@ -1,5 +1,3 @@
-time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
-
 # Run tmux if exists
 if command -v tmux>/dev/null; then
 	[ -z $TMUX ] && exec tmux
@@ -8,6 +6,5 @@ else
 fi
 
 echo "Updating configuration"
-#(cd ~/bundle/My-Sweet-Little-Sys/Nix/dotfile && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
 (cd ~/bundle/My-Sweet-Little-Sys && git pull && git submodule update --init --recursive)
-source ~/bundle/My-Sweet-Little-Sys/Nix/dotfile/zsh/zshrc.sh
+source ~/bundle/My-Sweet-Little-Sys/nix/dotfile/zsh/zshrc.sh
