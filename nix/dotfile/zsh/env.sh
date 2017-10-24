fdf801b05d14a7bea43e80fe8d1f230ac486db3b
Kiran Kumar Roy <kkroy22@gmail.com>
2017-10-24 10:00:16 +0530

    added again

---
+++ nix/dotfile/zsh/env.sh
@@ -0,0 +1,14 @@
+# SDK
+
+JAVA=/opt/sdk/jdk/jdk-9.0.1
+SCALA=/opt/sdk/scala
+NODE=/opt/sdk/node
+RUST=/opt/sdk/rust
+
+# IDE
+ATOM=/opt/ide/atom/atom-1.21.1-amd64
+
+# SCRIPTS
+SCRIPT=/home/analytics/bundle/script
+
+PATH=$PATH:$JAVA/bin:$SCALA/bin:$NODE/bin:$RUST/rustc/bin:$RUST/cargo/bin:$SCRIPT
