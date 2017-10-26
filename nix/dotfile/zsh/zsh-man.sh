41e9828a3e1d34759b66d9435e1cc4d4a43f3513
Kiran Kumar Roy <bikky22@users.noreply.github.com>
2017-10-26 12:07:18 +0530

    Update zsh-man.sh

--- nix/dotfile/zsh/zsh-man.sh
+++ nix/dotfile/zsh/zsh-man.sh
@@ -7,7 +7,7 @@
 #fi
 
 
-export ZSH=/home/test1/bundle/My-Sweet-Little-Sys/nix/dotfile/zsh/plugins/oh-my-zsh
+export ZSH=~/bundle/My-Sweet-Little-Sys/nix/dotfile/zsh/plugins/oh-my-zsh
 
 # https://www.bikky.space
 # https://www.owcsx.science
@@ -21,7 +21,7 @@ ZSH_THEME="robbyrussell"
 
 plugins=(git)
 
-source ~/bundle/My-Sweet-Little-Sys/nix/dotfile/zsh/plugins/oh-my-zsh/oh-my-zsh.sh
+source $ZSH/oh-my-zsh.sh
 
 echo "Updating configuration"
 #(cd ~/bundle/My-Sweet-Little-Sys && git pull && git submodule update --init --recursive)
