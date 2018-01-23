0c51d75e47de1504dd30907e6d8246a874ef0e80
Kiran Kumar Roy <bikky22@users.noreply.github.com>
2018-01-23 21:53:24 +0530

    Update root.sh

--- nix/root.sh
+++ nix/root.sh
@@ -1,53 +1,13 @@
 #! /bin/bash
 
-prompt_install() {
-	echo -n "$1 is not installed. Would you like to install $1 (y/n) " >&2
-	old_stty_cfg=$(stty -g)
-	stty raw -echo
-	answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-	stty $old_stty_cfg && echo
-	if echo "$answer" | grep -iq "^y" ;then
-		# This could def use community support
-		if [ -x "$(command -v apt)" ]; then
-			sudo apt install $1 -y
-
-		elif [ -x "$(command -v brew)" ]; then
-			brew install $1
-
-		elif [ -x "$(command -v pkg)" ]; then
-			sudo pkg install $1
-
-		elif [ -x "$(command -v pacman)" ]; then
-			sudo pacman -S $1
-
-		else
-			echo "I'm not sure what your package manager is!" 
-		fi 
-	fi
-}
-
-# https://www.bikky.space
-# https://www.owcsx.science
-# https://www.linkedin.com/in/kkroy22/
-
-check_for_software() {
-	echo "Checking to see if $1 is installed"
-	if ! [ -x "$(command -v $1)" ]; then
-		prompt_install $1
-	else
-		echo "$1 is installed."
-	fi
-}
-
 pkgs= (
 	'chromium-browser'
         'git'
 	'tmux'
 	'vim'
-	'python-virtualenv'
+	'python3-virtualenv'
 	'zsh'
 )
-
 
 # https://www.bikky.space
 # https://www.owcsx.science
