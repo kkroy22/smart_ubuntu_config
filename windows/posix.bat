44ec220201dd1fba7efe917e17ad0ec1bea4f700
Kiran Kumar Roy <bikky22@users.noreply.github.com>
2018-05-27 17:23:53 +0530

    Create posix.bat

---
+++ windows/posix.bat
@@ -0,0 +1,5 @@
+echo off
+set PATH=C:\Program Files\Git\mingw64_gcc\mingw64\bin;C:\Program Files\Git\bin;%PATH%
+rem echo %PATH%
+rem cd "C:\Program Files\Git\mingw64_gcc\mingw64\bin"
+"C:\WINDOWS\system32\cmd.exe"
