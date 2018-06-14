06daf57138ad0a82c801cb8ededcb1ac7412d9d5
Kiran Kumar Roy <bikky22@users.noreply.github.com>
2018-06-14 19:55:41 +0530

    Update posix.bat

--- windows/posix.bat
+++ windows/posix.bat
@@ -1,4 +1,5 @@
 echo off
 set PATH=C:\Program Files\Git\mingw64_gcc\mingw64\bin;C:\Program Files\Git\bin;%PATH%
 echo Posix on Windows by www.owcsx.science
+echo Run bash for linux Shell
 "C:\WINDOWS\system32\cmd.exe"
